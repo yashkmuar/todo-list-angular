798a68c16437ddcf2803cbfc4271d5bf7c2703ea
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-09 12:04:47 +0530

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todo-list-angular');
+  task="";
+  taskList:{id:number, task:string}[]=[];
 }
