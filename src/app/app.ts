d3fedfb6d01ae00e4300a090049a091fc4d85990
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-09 12:09:59 +0530

    taskList binded using two way with the html page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   //imports: [RouterOutlet],
+  imports:[FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -11,4 +13,10 @@ export class App {
   protected readonly title = signal('todo-list-angular');
   task="";
   taskList:{id:number, task:string}[]=[];
+
+  addTask(){
+    this.taskList.push({id:this.taskList.length+1, task:this.task});
+
+    console.log(this.taskList);
+  }
 }
